0e30ff5caba8720e6034dffa5eb8b00257c29c5f
Matt Santos <mattsantos541@gmail.com>
2022-02-16 11:56:12 -0600

    Change Df

--- Create Pitchers DF.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df= pd.read_csv("Pitching_2019.csv")
-
-df= pd.DataFrame(df)
-
--- create batters DF.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df= pd.read_csv("Batting_2019.csv")
-
-df= pd.DataFrame(df)
-
-df.info()
